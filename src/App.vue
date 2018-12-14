f7a2b7193e449f398d865eb05d5248a4be8edc4d
Mark Chavez <markchav3z@gmail.com>
2018-12-14 23:06:52 +0800

    Add github star button

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div>
     <header class="p-4 mb-8 border-b-2 border-black text-center">
       <h1 class="font-normal">{{appTitle}}</h1>
-      <p> Check out how much Stellar tokens your services have! </p>
+      <p class="mb-3"> Check out how much Stellar tokens your services have! </p>
+      <!-- Place this tag where you want the button to render. -->
+      <a class="github-button" href="https://github.com/mrkjlchvz/stellar-balances-checker" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star mrkjlchvz/stellar-balances-checker on GitHub">Star</a>
     </header>
 
     <div>
